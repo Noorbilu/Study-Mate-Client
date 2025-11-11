ae7613f32a2bde8607ac79ca11971d0897320e13
Noorbilu <nuralabonno@gmail.com>
2025-11-11 13:33:33 +0600

    added spinner file

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        
+<div className="min-h-[40vh] grid place-items-center"> 
+    <span className="loading loading-spinner loading-lg text-primary"></span> 
+    </div>
+        
+    );
+};
+
+export default Spinner;
